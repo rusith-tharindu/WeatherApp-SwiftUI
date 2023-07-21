654b37ca3c8649ea50b7b73103398955bc4a28b4
Rusith Tharindu Athukorala <103258178+rusith-tharindu@users.noreply.github.com>
2023-07-21 10:04:28 +0530

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Rusith Tharindu  on 2023-07-21.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
